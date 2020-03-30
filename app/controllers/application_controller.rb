78149142f6cae7902c24eede243c332c6f2308c3
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-03-30 08:33:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,24 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+get '/recipes' do 
+  @recipes = Recipe.all
+  erb :index
+end
 
-  # code actions here!
+get '/recipes/new' do 
+  erb :new
+end
 
+post '/recipes' do
+  @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+  redirect to "/articles/#{@recipe.id}"
+end
+
+
+get '/recipes/:id' do
+  @recipe = Recipe.find_by_id(params[:id])
+  erb :show
+end
+  
 end
